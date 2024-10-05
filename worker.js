3fcb5a46cae1a8b6ede65e77dc97a50d48e7e247
karxg <karim4420@aol.com>
2024-10-06 01:31:56 +0300

    proj

--- worker.js
+++ worker.js
@@ -60,22 +60,4 @@ userQueue.process(async (job, done) => {
   if (!user) {
     throw new Error('User not found');
   }
-  console.log(`Welcome ${user.email}!`);
-  try {
-    const mailSubject = 'Welcome to ALX-Files_Manager by B3zaleel';
-    const mailContent = [
-      '<div>',
-      '<h3>Hello {{user.name}},</h3>',
-      'Welcome to <a href="https://github.com/B3zaleel/alx-files_manager">',
-      'ALX-Files_Manager</a>, ',
-      'a simple file management API built with Node.js by ',
-      '<a href="https://github.com/B3zaleel">Bezaleel Olakunori</a>. ',
-      'We hope it meets your needs.',
-      '</div>',
-    ].join('');
-    Mailer.sendMail(Mailer.buildMessage(user.email, mailSubject, mailContent));
-    done();
-  } catch (err) {
-    done(err);
-  }
 });
